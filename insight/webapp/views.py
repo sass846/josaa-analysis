e22bd29aa4da47f9a1ea585f6b11e98436c17707
sass846 <skiran.self@gmail.com>
2024-06-14 23:48:22 +0530

    update institutes.html data

--- insight/webapp/views.py
+++ insight/webapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.paginator import Paginator
+from .models import Institute, josaa
 
 def index(request):
     return render(request, 'webapp/index.html')
@@ -11,10 +12,15 @@ def contact(request):
     return render(request, 'webapp/contact.html')
 
 def institutes(request):
-    return render(request, 'webapp/institutes.html')
+    institutes = Institute.objects.all()
+    paginator = Paginator(institutes, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'webapp/institutes.html', {'page_obj': page_obj})
 
 def seats(request):
     return render(request, 'webapp/seats.html')
 
 def analysis(request):
-    return render(request, 'webapp/analysis.html')
+    josaa_data = josaa.objects.all()
+    return render(request, 'webapp/analysis.html', {'josaa': josaa_data})

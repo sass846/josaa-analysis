f39c3200bef7b04f53a215ffe135e00643b03b0c
sass846 <skiran.self@gmail.com>
2024-06-14 22:00:32 +0530

    create model josaa

--- insight/webapp/models.py
+++ insight/webapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class josaa(models.Model):
+    institute = models.CharField(max_length=255)
+    branch = models.CharField(max_length=255)
+    quota = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    gender = models.CharField(max_length=255)
+    opening_rank = models.IntegerField()
+    closing_rank = models.IntegerField()
+    preparatory = models.BooleanField()

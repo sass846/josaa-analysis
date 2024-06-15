23c37b8cdf86f6555fd5bf6b6f6e424baff6e6e8
sass846 <skiran.self@gmail.com>
2024-06-16 02:19:16 +0530

    update analysis

--- insight/webapp/views.py
+++ insight/webapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
+# from django.core import serializers
 from .models import Institute, josaa
 
 def index(request):
@@ -29,4 +30,23 @@ def seats(request):
 
 def analysis(request):
     josaa_data = josaa.objects.all()
-    return render(request, 'webapp/analysis.html', {'josaa': josaa_data})
+    institutes = josaa.objects.values_list('institute', flat=True).distinct()
+
+    # institute_type = request.GET.get('institute_type')
+    # if institute_type:
+    #     josaa_data = josaa_data.filter(institute_type=institute_type)
+
+    institute_name = request.GET.get('institute_name')
+    if institute_name:
+        josaa_data = josaa_data.filter(institute=institute_name)
+
+    category = request.GET.get('category')
+    if category:
+        josaa_data = josaa_data.filter(category=category)
+
+    gender = request.GET.get('gender')
+    if gender:
+        josaa_data = josaa_data.filter(gender=gender)
+
+    # serialized_data = serializers.serialize('json', josaa_data)
+    return render(request, 'webapp/analysis.html', {'institutes':institutes, 'data': josaa_data})
